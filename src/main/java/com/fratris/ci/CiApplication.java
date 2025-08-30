f41963e623093d476e713407c67c0feb53b93c37
Eduardo <eduardofrad.dev@gmail.com>
2025-08-30 17:55:49 -0300

    change

--- src/main/java/com/fratris/ci/CiApplication.java
+++ src/main/java/com/fratris/ci/CiApplication.java
@@ -8,7 +8,7 @@ public class CiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiApplication.class, args);
-		System.out.printf("Result: " + sum(10.0, 10.0));
+		System.out.printf("Result sum: " + sum(10.0, 10.0));
 	}
 
 	public static Double sum(Double arg1, Double arg2){
